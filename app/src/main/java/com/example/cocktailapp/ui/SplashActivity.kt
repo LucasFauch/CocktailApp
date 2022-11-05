e96936acefbde3f8531f189809a27cc124a3bca9
Lucas Fauchereau <lucas.fauch@gmail.com>
2022-11-05 17:37:20 -0400

    Splash screen

---
+++ app/src/main/java/com/example/cocktailapp/ui/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.cocktailapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.cocktailapp.ui.theme.CocktailAppTheme
+import kotlinx.coroutines.delay
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            delay(3000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        setContent {
+            CocktailAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text("Cocktail App splash screen")
+                }
+            }
+        }
+    }
+}
