9621f39c4c5ea0278f6938d5749467d1d4ec2a53
Quentin Pauwels <quentin.pauwels@hotmail.com>
2022-11-08 05:43:26 +0100

    Add test color and fontsize on Friends menu

--- app/src/main/java/com/example/cocktailapp/ui/screens/FriendsScreen.kt
+++ app/src/main/java/com/example/cocktailapp/ui/screens/FriendsScreen.kt
@@ -1,10 +1,25 @@
 package com.example.cocktailapp.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun Friends(navController: NavController){
-    Text("Friends")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        Text(
+            text = "Liste Friends",
+            color = MaterialTheme.colors.primaryVariant,
+            fontSize = MaterialTheme.typography.h3.fontSize
+        )
+    }
 }
